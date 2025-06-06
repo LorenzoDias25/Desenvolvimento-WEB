a23a08b9bedec81ba8d54dd569765557568af52f
Lorenzo Dias <137002157+LorenzoDias25@users.noreply.github.com>
2025-06-06 15:35:00 -0300

    feito

--- TarefasJS2/Tarefa15/app.js
+++ TarefasJS2/Tarefa15/app.js
@@ -1,7 +1,12 @@
-botao = document.querySelector("button");
-msg = document.querySelector("h2");
+const botaoEnviar = document.getElementById('enviar');
 
-botao.addEventListener("click", function () {
-  const date = new Date();
-  msg.innerHTML = `Data: ${date};`;
+botaoEnviar.addEventListener('click', function() {
+  const nome = document.getElementById('formNome').value;
+  const email = document.getElementById('formEmail').value;
+  const msg = document.getElementById('formMsg').value;
+
+  document.querySelector('div').style.visibility = 'visible';
+  document.getElementById('nome').innerHTML = `Nome: ${nome}`;
+  document.getElementById('email').innerHTML = `E-mail: ${email}`;
+  document.getElementById('msg').innerHTML = `Mensagem: ${msg}`;
 });
